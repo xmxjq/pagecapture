8879241439b5801c65c0dddf18da4045e192f1ac
imhmao <imhmao@16b99238-1c9a-11df-9fc2-77fecae625a6>
2010-04-07 04:31:33 +0000

    过滤页面Title中的不符合文件名规范的字符

--- trunk/PageCapture/CBho/CBho_i.c
+++ trunk/PageCapture/CBho/CBho_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Thu Mar 11 12:05:56 2010
+/* at Sun Apr 04 15:29:06 2010
  */
 /* Compiler settings for CBho.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
