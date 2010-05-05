7d8f136843b19597bd31c9cbb4fd3cdbe15ee208
imhmao <imhmao@16b99238-1c9a-11df-9fc2-77fecae625a6>
2010-05-05 09:45:30 +0000

    1.0

--- trunk/PageCapture/CBho/CBho_i.c
+++ trunk/PageCapture/CBho/CBho_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Apr 04 15:29:06 2010
+/* at Wed May 05 17:30:38 2010
  */
 /* Compiler settings for CBho.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
